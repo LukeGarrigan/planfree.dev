8ae013c28eddadf98d19f4fcbf5f68121d8f3375
Luke <Lukegarrigan8@gmail.com>
2021-06-15 07:39:38 +0100

    Update Game.vue

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -38,7 +38,7 @@
 import store from '@/store';
 import { Component, Vue } from 'vue-property-decorator';
 import Modal from '@/components/Modal.vue'; 
-import Player from '@/view-models/Player'; 
+import Player from '@/view-models/player'; 
 import { io } from "socket.io-client";
 @Component({
   components: {

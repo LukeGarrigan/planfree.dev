5134b99e91b43e9ca2f820fe9fb59b7fd97b138f
Tonio Ercegović <te51681@fer.hr>
2021-07-09 22:23:58 +0200

    added confirm button to modal which shown when screen size is less than 800px

--- client/src/components/Modal.vue
+++ client/src/components/Modal.vue
@@ -4,6 +4,13 @@
       <span>{{title}}</span>
 
       <input id="selectNameInput" v-model="name" type="text" class="input" @keypress.enter="completed"/>
+      <button
+        class="button"
+        :class="{ disabled: !name }"
+        @click="completed"
+      >
+        <span>Confirm</span>
+      </button>
       
     </div>
   </div>
@@ -23,7 +30,8 @@ export default class Modal extends Vue {
   }
 
   public completed() {
-    this.$emit('completed', this.name);
+    if(this.name && this.name != "")
+      this.$emit('completed', this.name);
   }
 }
 </script>
@@ -70,6 +78,64 @@ export default class Modal extends Vue {
     font-family: "Montserrat", sans-serif;
     font-size: 26px;
     font-weight: semibold;
+  }
+
+  .button {
+    user-select: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 9999;
+    width: 90px;
+    height: 40px;
+    margin-top: 0.5em;
+    background: #f3f0f1;
+    border-radius: 32px;
+    text-align: center;
+    border: none;
+    cursor: pointer;
+    transition: all 0.1s ease-in-out;
+    box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
+      6px 6px 10px rgba(0, 0, 0, 0.2);
+    color: #161b1f;
+    &:hover {
+      opacity: 0.3;
+      box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
+        6px 6px 10px rgba(0, 0, 0, 0.2);
+    }
+    &:active {
+      opacity: 1;
+      box-shadow: inset -4px -4px 8px rgba(255, 255, 255, 0.5),
+        inset 8px 8px 16px rgba(0, 0, 0, 0.1);
+    }
+    &:focus {
+      outline: none;
+    }
+
+    span {
+      line-height: 100px;
+      font-family: "Montserrat", sans-serif;
+      font-size: 18px;
+      font-weight: semibold;
+    }
+  }
+
+  .disabled {
+    opacity: 1;
+    box-shadow: inset -4px -4px 8px rgba(255, 255, 255, 0.5),
+      inset 8px 8px 16px rgba(0, 0, 0, 0.1);
+
+    &:hover {
+      opacity: 1;
+      box-shadow: inset -4px -4px 8px rgba(255, 255, 255, 0.5),
+        inset 8px 8px 16px rgba(0, 0, 0, 0.1);
+    }
+  }
+
+  @media only screen and (min-width: 800px) {
+    .button {
+      display: none;
+    }
   }
 
 </style>

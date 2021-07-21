1f43cf86a9da497eecd60cf8f542e1e12a38e513
Luke <Lukegarrigan8@gmail.com>
2021-07-21 08:22:44 +0100

    Updated favicon

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -11,7 +11,7 @@
     <button v-if="!modal && playerHasVoted() && !showVotes" class="button" @click="showVotesClicked()"><span>Show votes!</span></button>
     <button v-if="!modal && showVotes && countdown == 0" class="button start" @click="startNewGame()"><span>Start new game!</span></button>
     <button v-if="!modal && showVotes && countdown > 0" class="button no-hover"><span>{{countdown}}</span></button>
-    <Modal v-if="modal" title="What is your name?" @completed="enteredName"></Modal>
+    <Modal v-if="modal" title="Choose your display name" @completed="enteredName"></Modal>
 
     <div class="players" v-for="player in getPlayers()" :key="player.id">
       <div class="player" :class="{'voted': player.vote}">

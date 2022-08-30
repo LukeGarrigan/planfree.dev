019529c9b85fa5cb23ee2ca219e5b4c7e23870e6
Luke <lukegarrigan8@gmail.com>
2022-08-30 08:45:09 +0100

    Update service-worker.js

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/6.5.4/workbox-sw.js');
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/6.5.4/workbox-sw.min.js');
 
 workbox.routing.registerRoute(
   ({request}) => request.destination === 'image',

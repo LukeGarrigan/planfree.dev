457672892e670b47017c175432c55d302ac212ed
Luke <lukegarrigan8@gmail.com>
2024-01-15 09:00:25 +0000

    Remove console.log, for the score, find the average then the fib most similar

--- client/src/composables/useTickets.ts
+++ client/src/composables/useTickets.ts
@@ -7,7 +7,6 @@ export function useTickets() {
     const votingOnName = computed(() => tickets.value.find((f: Ticket) => f.votingOn === true)?.name);
 
     function ticketUpdated() {
-        console.log('sending up ', tickets.value);
         socket.value.emit('ticket', tickets.value);
     }
     return {tickets, votingOnId, votingOnName, ticketUpdated};

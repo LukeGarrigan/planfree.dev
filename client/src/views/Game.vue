c808493cb756c4c73389ad6899b97a5a762b7d36
Luke <lukegarrigan8@gmail.com>
2024-01-16 08:10:31 +0000

    added closest rather than mode

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -3,76 +3,83 @@
     <h1 class="screen-reader-only">Planfree.dev game lobby</h1>
 
     <Modal
-      v-if="modal"
-      title="Choose your display name"
-      @completed="enteredName"
+        v-if="modal"
+        title="Choose your display name"
+        @completed="enteredName"
     ></Modal>
 
     <div v-if="!modal" class="home">
       <div class="top-buttons">
-      
+
 
         <button class="edit-name-button" @click="modal = true">
           <div>{{ name }}</div>
           <div>
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 0 24 24"
-              width="24px"
-              fill="#000000"
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 0 24 24"
+                width="24px"
+                fill="#000000"
             >
-              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path d="M0 0h24v24H0V0z" fill="none"/>
               <path
-                d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z"
+                  d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z"
               />
             </svg>
           </div>
         </button>
         <button
-          v-if="!showCopiedToClipboard"
-          class="button invite"
-          @click="copyToClipboard()"
+            v-if="!showCopiedToClipboard"
+            class="button invite"
+            @click="copyToClipboard()"
         >
           <div>Invite players</div>
           <div>
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 0 24 24"
-              width="24"
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24"
             >
-              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path d="M0 0h24v24H0V0z" fill="none"/>
               <path
-                d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
+                  d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
               />
             </svg>
           </div>
         </button>
         <button
-          v-if="!modal && showCopiedToClipboard"
-          class="button invite copied no-hover"
+            v-if="!modal && showCopiedToClipboard"
+            class="button invite copied no-hover"
         >
           <div>Copied to clipboard</div>
           <div></div>
         </button>
         <button class="fib-button" @click="toggleTickets">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h440l200 200v440q0 33-23.5 56.5T760-120H200Zm0-80h560v-400H600v-160H200v560Zm80-80h400v-80H280v80Zm0-320h200v-80H280v80Zm0 160h400v-80H280v80Zm-80-320v160-160 560-560Z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+            <path
+                d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h440l200 200v440q0 33-23.5 56.5T760-120H200Zm0-80h560v-400H600v-160H200v560Zm80-80h400v-80H280v80Zm0-320h200v-80H280v80Zm0 160h400v-80H280v80Zm-80-320v160-160 560-560Z"/>
+          </svg>
         </button>
       </div>
 
       <div class="top-left">
         <button class="star-button" @click="goToGithub()" aria-label="Go to the GitHub repository">
           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
-            <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6c0-0.4,0-0.9,0.2-1.3 C7.2,6.1,7.4,6,7.5,6c0,0,0.1,0,0.1,0C8.1,6.1,9.1,6.4,10,7.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3c0.9-0.9,2-1.2,2.5-1.3 c0,0,0.1,0,0.1,0c0.2,0,0.3,0.1,0.4,0.3C17,6.7,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4 c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3C22,6.1,16.9,1.4,10.9,2.1z"></path>
+            <path
+                d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6c0-0.4,0-0.9,0.2-1.3 C7.2,6.1,7.4,6,7.5,6c0,0,0.1,0,0.1,0C8.1,6.1,9.1,6.4,10,7.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3c0.9-0.9,2-1.2,2.5-1.3 c0,0,0.1,0,0.1,0c0.2,0,0.3,0.1,0.4,0.3C17,6.7,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4 c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3C22,6.1,16.9,1.4,10.9,2.1z"></path>
           </svg>
         </button>
 
         <button v-if="showInstallPwa" class="star-button" @click="installPWA()" aria-label="Add to homescreen">
-          <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M7 17v-2H3.5q-.625 0-1.062-.438Q2 14.125 2 13.5v-9q0-.625.438-1.062Q2.875 3 3.5 3H11v1.5H3.5v9h13V11H18v2.5q0 .625-.438 1.062Q17.125 15 16.5 15H13v2Zm7.479-6L11 7.521l1.062-1.063 1.688 1.688V3h1.5v5.104l1.688-1.687L18 7.479Z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20">
+            <path
+                d="M7 17v-2H3.5q-.625 0-1.062-.438Q2 14.125 2 13.5v-9q0-.625.438-1.062Q2.875 3 3.5 3H11v1.5H3.5v9h13V11H18v2.5q0 .625-.438 1.062Q17.125 15 16.5 15H13v2Zm7.479-6L11 7.521l1.062-1.063 1.688 1.688V3h1.5v5.104l1.688-1.687L18 7.479Z"/>
+          </svg>
         </button>
         <div class="voting-on" v-if="votingOnName">
-          <p class="voting-on-label">Voting on: <b>{{votingOnName}}</b></p>
+          <p class="voting-on-label">Voting on: <b>{{ votingOnName }}</b></p>
         </div>
       </div>
 
@@ -80,16 +87,16 @@
         <span>Cast your votes</span>
       </button>
       <button
-        v-if="playerHasVoted() && !showVotes"
-        class="button"
-        @click="showVotesClicked()"
+          v-if="playerHasVoted() && !showVotes"
+          class="button"
+          @click="showVotesClicked()"
       >
         <span>Show votes!</span>
       </button>
       <button
-        v-if="showVotes && countdown === 0"
-        class="button start"
-        @click="startNewGame()"
+          v-if="showVotes && countdown === 0"
+          class="button start"
+          @click="startNewGame()"
       >
         <span>Start new game!</span>
       </button>
@@ -108,7 +115,7 @@
 
       <div class="options" v-if="!showVotes || (showVotes && countdown !== 0)">
         <button
-          v-for="vote in [
+            v-for="vote in [
             '0',
             '1',
             '2',
@@ -122,11 +129,11 @@
             '89',
             '?',
           ]"
-          :key="`vote-${vote}`"
-          class="fib-button"
-          :class="{ current: currentVote === vote }"
-          @click="performVote(vote)"
-          :disabled="currentVote === vote"
+            :key="`vote-${vote}`"
+            class="fib-button"
+            :class="{ current: currentVote === vote }"
+            @click="performVote(vote)"
+            :disabled="currentVote === vote"
         >
           <span>{{ vote }}</span>
         </button>
@@ -134,7 +141,7 @@
       <div class="results-container" v-if="showVotes && countdown === 0">
         <div class="results">
           <div class="average">Average: {{ getAverage() }}</div>
-          <div class="popular">Popular: {{ getMode() }}</div>
+          <div class="popular">Closest: {{ getClosest() }}</div>
         </div>
       </div>
       <div class="tickets" v-show="showTickets">
@@ -147,9 +154,9 @@
 <script setup lang="ts">
 import Modal from "@/components/Modal.vue";
 import Player from "@/view-models/player";
-import { io } from "socket.io-client";
-import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
+import {io} from "socket.io-client";
+import {computed, onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
 import Tickets from "@/components/Tickets.vue";
 import {useTickets} from "@/composables/useTickets";
 import {useGameEngine} from "@/composables/useGameEngine";
@@ -159,8 +166,8 @@ const modal = ref(true);
 const showCopiedToClipboard = ref(false);
 const name = ref("");
 const showTickets = ref(false);
-const { votingOnName } = useTickets();
-const { socket, setSocket, players, showVotes, countdown, currentVote} = useGameEngine();
+const {votingOnName} = useTickets();
+const {socket, setSocket, players, showVotes, countdown, currentVote} = useGameEngine();
 
 let deferredPrompt: any;
 window.addEventListener('beforeinstallprompt', (e) => {
@@ -216,7 +223,7 @@ function enteredName(updatedName: string) {
 
 function playerHasVoted() {
   return (
-    players.value.filter((p: Player) => p.vote !== null && p.vote !== undefined).length > 0
+      players.value.filter((p: Player) => p.vote !== null && p.vote !== undefined).length > 0
   );
 }
 
@@ -231,7 +238,7 @@ function copyToClipboard() {
   setTimeout(() => (showCopiedToClipboard.value = false), 3000);
 }
 
-function getAverage() {
+const average = computed(() => {
   let count = 0;
   let total = 0;
   for (const player of players.value) {
@@ -240,36 +247,25 @@ function getAverage() {
       count++;
     }
   }
-  return (total / count).toFixed(1).replace(/\.0+$/, "");
+  return total / count;
+})
+
+function getAverage() {
+  return (average.value).toFixed(1).replace(/\.0+$/, "");
 }
 
-function getMode(): string {
-  const scores: Record<string, number> = {};
-  for (const player of players.value) {
-    if (player.vote) {
-      if (scores[player.vote]) {
-        scores[player.vote] = scores[player.vote] + 1;
-      } else {
-        scores[player.vote] = 1;
-      }
+function getClosest() {
+  const fib = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  let closest = 0;
+  let smallestDiff = Number.MAX_VALUE;
+  for (const number of fib) {
+    const difference = Math.abs(number - average.value);
+    if (difference < smallestDiff) {
+      smallestDiff = difference;
+      closest = number;
     }
   }
-
-  let mostSeen = 1;
-  let mostSeenCard = "";
-  for (const key in scores) {
-    const value = scores[key];
-    if (value > mostSeen) {
-      mostSeen = value;
-      mostSeenCard = key;
-    }
-  }
-
-  if (mostSeen == 1) {
-    return "NA";
-  } else {
-    return mostSeenCard;
-  }
+  return closest;
 }
 
 function goToGithub() {
@@ -279,12 +275,11 @@ function goToGithub() {
 function joiningAGame() {
   const currentState = socket.value;
   return (
-    currentState &&
-    Object.keys(currentState).length === 0 &&
-    currentState.constructor === Object
+      currentState &&
+      Object.keys(currentState).length === 0 &&
+      currentState.constructor === Object
   );
 }
-
 
 
 const toggleTickets = () => showTickets.value = !showTickets.value;
@@ -301,6 +296,7 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
   .player {
     border-radius: 26px;
     border: none;
@@ -309,12 +305,13 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
     height: 80px;
     background: #f3f0f1;
     box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
-      6px 6px 10px rgba(0, 0, 0, 0.2);
+    6px 6px 10px rgba(0, 0, 0, 0.2);
     color: #161b1f;
     display: flex;
     justify-content: center;
     align-items: center;
   }
+
   .name {
     margin-top: 1em;
     text-align: center;
@@ -333,6 +330,7 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
   width: 100%;
   box-sizing: border-box;
 }
+
 .button {
   display: flex;
   align-items: center;
@@ -349,17 +347,19 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
   cursor: pointer;
   transition: all 0.1s ease-in-out;
   box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
-    6px 6px 10px rgba(0, 0, 0, 0.2);
+  6px 6px 10px rgba(0, 0, 0, 0.2);
   color: #161b1f;
+
   &:hover {
     opacity: 0.3;
     box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
-      6px 6px 10px rgba(0, 0, 0, 0.2);
+    6px 6px 10px rgba(0, 0, 0, 0.2);
   }
+
   &:active {
     opacity: 1;
     box-shadow: inset -4px -4px 8px rgba(255, 255, 255, 0.5),
-      inset 8px 8px 16px rgba(0, 0, 0, 0.1);
+    inset 8px 8px 16px rgba(0, 0, 0, 0.1);
   }
 
 }
@@ -381,16 +381,17 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
     width: 30px;
     height: 30px;
     box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
-      6px 6px 10px rgba(0, 0, 0, 0.2);
+    6px 6px 10px rgba(0, 0, 0, 0.2);
     color: #161b1f;
 
     &:hover {
       opacity: 0.3;
       box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
-        6px 6px 10px rgba(0, 0, 0, 0.2);
+      6px 6px 10px rgba(0, 0, 0, 0.2);
     }
 
   }
+
   .voting-on {
     font-family: "Montserrat", sans-serif;
     margin-left: 20px;
@@ -420,6 +421,7 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
   top: 2%;
   right: 2%;
   gap: 20px;
+
   .invite {
     position: relative;
     user-select: none;
@@ -427,6 +429,7 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
     width: 300px;
     height: 70px;
     font-size: 26px;
+
     svg {
       position: relative;
       left: 2px;
@@ -449,9 +452,11 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
     gap: 10px;
     opacity: 0.5;
     cursor: pointer;
+
     &:hover {
       opacity: 1;
     }
+
     &:hover::before {
       transform: scaleX(1);
     }
@@ -468,6 +473,7 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
       transform: scaleX(0);
       transition: transform 0.3s ease;
     }
+
     svg {
       position: relative;
       left: 2px;
@@ -479,7 +485,7 @@ const toggleTickets = () => showTickets.value = !showTickets.value;
 .screen-reader-only {
   position: absolute;
   width: 0px;
-  overflow:hidden;
+  overflow: hidden;
 }
 
 .copied {
@@ -521,11 +527,12 @@ span {
     height: 100px;
     transition: all 0.1s ease-in-out;
     box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
-      6px 6px 10px rgba(0, 0, 0, 0.2);
+    6px 6px 10px rgba(0, 0, 0, 0.2);
 
     user-select: none;
     font-family: "Montserrat", sans-serif;
     font-weight: semibold;
+
     &:focus {
       outline: none;
     }
@@ -563,18 +570,21 @@ span {
   box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
   6px 6px 10px rgba(0, 0, 0, 0.2);
   color: #161b1f;
+
   &:not(.current) {
     &:hover {
       opacity: 0.3;
       box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8),
       6px 6px 10px rgba(0, 0, 0, 0.2);
     }
+
     &:active {
       opacity: 1;
       box-shadow: inset -4px -4px 8px rgba(255, 255, 255, 0.5),
       inset 8px 8px 16px rgba(0, 0, 0, 0.1);
     }
   }
+
   &.current {
     background: #54e8dd;
   }

eba5a2be1ed7d24ba9666b88345be40df6926061
Luke <lukegarrigan8@gmail.com>
2025-01-17 08:26:38 +0000

    underlined links were a bit ugly

--- client/src/components/NavigationBar.vue
+++ client/src/components/NavigationBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="navigation-bar">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/contact">Contact</router-link>
-    <router-link to="/privacy">Policy</router-link>
+    <router-link class="link" to="/">Home</router-link>
+    <router-link class="link" to="/about">About</router-link>
+    <router-link class="link" to="/contact">Contact</router-link>
+    <router-link class="link" to="/privacy">Policy</router-link>
   </div>
 </template>
 
@@ -20,7 +20,9 @@
   gap: 10px;
   margin-top: 10px;
   font-family: "Montserrat", sans-serif;
-  font-weight: semibold;
+  .link {
+    text-decoration: none;
+  }
 }
 
 </style>

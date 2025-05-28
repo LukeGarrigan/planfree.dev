fd1c10bc694d45b2c68406fa4494c6de01af2490
Luke Garrigan <lukegarrigan8@gmail.com>
2025-05-28 09:20:37 +0100

    Can't refresh page

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,9 +1,9 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 import Game from '../views/Game.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/',
